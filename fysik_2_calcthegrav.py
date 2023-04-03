c8dba664ec5fe4f7d62fec6bc87f205807dc0ccd
HugoZedendahl <hugo.zedendahl@gmail.com>
2023-04-03 13:56:52 +0000

    fysik 2 project started

--- fysik_2_calcthegrav.py
+++ fysik_2_calcthegrav.py
@@ -2,20 +2,39 @@
 import math
 
 
-# function for finding roots
-def equationroots(a, b, c):
+# function for finding g
+def findGFromPendulum(l, T, ocillations):
     z = b / c
     x = 4 * math.pi** 2
     print((x * a) / z ** 2)
 
-
-# inputs
-print("enter value for a")
-a = float(input())
-print("enter value for b")
-b = float(input())
-print("enter value for c")
-c = float(input())
+def findSwingTimeMathPendulum(l):
+    print ("Wip")
 
 
-equationroots(a, b, c)
+# input equation
+print("choose calculation")
+choice = int(input())
+
+if choice == 1:
+    print("enter value for length")
+    global l
+    l = float(input())
+    print("enter value for time")
+    global T
+    T = float(input())
+    print("enter value for ocillations")
+    global ocillations
+    ocillations = float(input())
+
+    print(findGFromPendulum(l, T, ocillations))
+
+elif choice == 2:
+    print("enter value for l")
+    global l
+    l = float(input())
+    print (findSwingTimeMathPendulum(a))
+
+elif choise == 0:
+    print("ending .py file")
+quit()
