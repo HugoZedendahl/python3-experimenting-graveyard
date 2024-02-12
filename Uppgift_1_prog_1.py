ea6744ec5f6f1ba6c841e20790d9c56d17b0780c
HugoZedendahl <hugo.zedendahl@gmail.com>
2024-02-12 16:17:49 +0000

    completed first assignment

---
+++ Uppgift_1_prog_1.py
@@ -0,0 +1,24 @@
+
+variableToConvert = ""
+#variabeln vi vill arbeta med
+while True:
+    print("skriv in en siffra, eller skriv [sluta] för att avsluta programmet")
+    variableToConvert = input()
+    #input för siffra
+    print("du skrev "+variableToConvert)
+    #visar strängen
+
+    if(variableToConvert == "sluta"):
+        quit()
+    else:
+        try:
+            variableToConvert = float(variableToConvert)
+            #översätter strängen till ett flyttal
+        except:
+            print("inte ett nummer eller sluta, försök igen")
+            continue
+
+    print(variableToConvert)
+    #översätter flyttalet till ett heltal
+    variableToConvert = int(variableToConvert)
+    print(variableToConvert)
