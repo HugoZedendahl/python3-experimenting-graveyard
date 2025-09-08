ea5e50d7d3a517384cefa3a8214c13693c6ce9f8
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2025-09-08 15:38:48 +0200

    exercise and vs code setup

---
+++ exercise_2_incorrect_method.py
@@ -0,0 +1,10 @@
+container = input("any number digits int: ")
+container = str(container)
+container = list(container)
+tempContainer = 0
+
+for x in container:
+    
+    tempContainer = tempContainer + int(x)
+
+print(tempContainer)
