dfb6b43a0545f55db01891ae0c72b2c5c08bd2e7
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2025-09-09 18:14:45 +0200

    started on advanced exercise

---
+++ the nine/the nine.py
@@ -0,0 +1,18 @@
+matrix = ([],[],[])
+wordList = open("the nine.txt", "r").readlines()
+while True:
+    inputLetters = input("Nian: ")
+    if inputLetters.conunt() == 9:
+        inputLetters = str.lower(inputLetters)
+        break
+    elif inputLetters.conunt() > 9:
+        print("Fel: för många bokstäver, försök igen.")
+    else:
+        print("Fel: för få bokstäver, försök igen.")
+
+matrix[0] = list(inputLetters[0:3])
+matrix[1] = list(inputLetters[3:6])
+matrix[2] = list(inputLetters[6:9])
+specialLetter = matrix[1][1]
+
+print(wordList)
