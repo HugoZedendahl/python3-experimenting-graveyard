68da6e0a121a160d3225f2a0bb4ada07356cbe0a
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2025-09-09 19:25:30 +0200

    ran this locally for testing purposes. could not get python to read the text file from url.

--- the nine/the nine.py
+++ the nine/the nine.py
@@ -1,18 +1,40 @@
-matrix = ([],[],[])
+#import random
+#import string
+#the code in comments are for testing purposes
+matrix = []
 wordList = open("svenskaOrd.txt", "r").readlines()
+outputList  = []
+specialOutputList = []
 while True:
     inputLetters = input("Nian: ")
-    if inputLetters.conunt() == 9:
-        inputLetters = str.lower(inputLetters)
-        break
-    elif inputLetters.conunt() > 9:
+    #def random_char(y):
+    #    return ''.join(random.choice(string.ascii_letters) for x in range(y))
+    #found this method to generate test strings here https://stackoverflow.com/questions/2823316/generate-a-random-letter-in-python
+    #inputLetters = random_char(9)
+    if len(inputLetters) == 9:
+        try:
+            inputLetters = str.lower(inputLetters)
+            break
+        except :
+            print("Fel: ogiltiga tecken, försök igen.")
+    elif len(inputLetters) > 9:
         print("Fel: för många bokstäver, försök igen.")
     else:
         print("Fel: för få bokstäver, försök igen.")
-
-matrix[0] = list(inputLetters[0:3])
-matrix[1] = list(inputLetters[3:6])
-matrix[2] = list(inputLetters[6:9])
-specialLetter = matrix[1][1]
-
-print(wordList)
+matrix = list(inputLetters)
+specialLetter = matrix[4]
+for x in wordList:
+    matches = 0
+    for y in matrix:
+        if y in x:
+            matches += 1
+    if x.count(specialLetter) > 0 and 9 > matches > 4:
+        outputList.append(x.strip())
+    elif x.count(specialLetter) > 0 and matches == 9:
+        specialOutputList.append(x.strip())
+for z in outputList:
+    print(z)
+print("")
+print(len(specialOutputList)," ord som använder alla bokstäver:")
+for zz in specialOutputList:
+    print(zz)
